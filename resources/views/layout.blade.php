cb4b7f65096ac4c28a78f526d00df06d3ba8b9c7
JoziasseK <jozi0018@hz.nl>
2021-03-29 14:58:12 +0200

    Basis code

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Healthy Region</title>
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}"/>
+
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="header">
+        <h1>Healthy Region</h1>
+        <p>Welkom op de site van lectoraat Healthy Region</p>
+    </div>
+
+
+    <!--Menu-->
+
+    <ul>
+        <li><a>Home</a></li>
+        <li><a>Bestanden</a></li>
+    </ul>
+
+    @yield('content')
+</div>
+</body>
+</html>
