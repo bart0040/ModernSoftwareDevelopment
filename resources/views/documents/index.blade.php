457f02f08b34502d0cc4c7ba9b8de0fa88bef5c0
bart0040 <bart0040@hz.nl>
2021-04-01 11:07:39 +0200

    onnodige info uit index pagina weggehaald

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -6,7 +6,6 @@
 
         <a href="/documents/create" class="">Add new document</a>
         <tr>
-            <td>ID</td>
             <td>Project Name</td>
             <td>Document Name</td>
             <td>Author</td>
@@ -14,21 +13,18 @@
             <td>Keywords</td>
             <td>Language</td>
             <td>Created at</td>
-            <td>Updated at</td>
             <td>Actions</td>
         </tr>
         </thead>
         <tbody>
         @foreach($documents as $document)
             <tr>
-                <td>{{ $document->id }}</td>
                 <td>{{ $document->project_name }}</td>
                 <td>{{ $document->document_name }}</td>
                 <td>{{ $document->author }}</td>
                 <td>{{ $document->document }}</td>
                 <td>{{ $document->keywords }}</td>
                 <td>{{ $document->language }}</td>
-                <td>{{ $document->created_at }}</td>
                 <td>{{ $document->updated_at }}</td>
                 <td><a href="{{ route('documents.edit', $document->id) }}">Edit</a></td>
             </tr>
