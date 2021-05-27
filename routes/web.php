66102ca313611c2e191fe21150cdd09be64c9dad
Lucvanwesten2003 <70520279+Lucvanwesten2003@users.noreply.github.com>
2021-05-27 15:14:33 +0200

    shit werkt plz

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DocumentController;
 use App\Http\Controllers\FilterController;
 use App\Http\Controllers\UploadController;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +19,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
 Route::resource('/documents', DocumentController::class);
 Route::resource('/filter', Filtercontroller::class, ['except' => ['create', 'show']]);
 Route::post('/showFiltered', [DocumentController::class, 'showFiltered']);
 
+require __DIR__.'/auth.php';
