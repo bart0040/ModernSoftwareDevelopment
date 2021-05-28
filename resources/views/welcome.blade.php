8f29d8d8f3ebc2e0051108ad3a04873c4af150bd
JustinSaaman <58013729+JustinSaaman@users.noreply.github.com>
2021-05-28 12:31:13 +0200

    Emptied welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,5 @@
 
 @section('content')
 
-    <div class="text">
-        <div class="wrapper">
-        <h1>Healthy Region<br></h1>
 
-        <p style="font-family: inherit">Toerisme, vitaliteit, bewegen, welzijn, gezondheid én Zeeland. Wat ontstaat er als je
-            raakvlakken tussen deze
-            onderwerpen opzoekt?</p>
-        <br>
-        <p><strong>Cross-overs gezondheid en toerisme</strong><br></p>
-        <p>Binnen het lectoraat Healthy Region wordt onderzoek gedaan naar cross-overs tussen gezondheid van burgers en
-            toeristen en kenmerkende omgevingsfactoren die aanwezig zijn in Zeeland. Onderzoekers, studenten,
-            ondernemers, bestuurders en gebruikers werken samen aan onderzoeksprojecten. Dit levert kennis op waarmee
-            diensten en producten – gerelateerd aan een gezond leven – beter afgestemd worden op veranderende behoeften
-            van zowel bewoners als toeristen in Zeeland.<br></p>
-
-        <p>Op deze manier draagt het lectoraat Healthy Region bij aan een slimme, duurzame en inclusieve groei van
-            toerisme en economie van Zeeland. Daarbij biedt het ook maatschappelijke waarde, omdat bewoners eveneens
-            gebruik kunnen maken van de innovatieve diensten en producten met betrekking op het gezonde leven.</p>
-        <p><strong>Healthy living, lifestyle, hospitality</strong></p>
-
-        <p>Drie thema’s zijn de rode draad in de onderzoeksprojecten van het lectoraat Healthy Region.</p>
-        <em>Healthy living<br></em>
-        <p>Hoe kunnen burgers een vitale leefstijl ontwikkelen en behouden in de gezonde regio? En welke activiteiten en
-            voorzieningen die gericht zijn op welzijn, gezondheid en vitaliteit dragen daar aan bij? En hoe kunnen zorg-
-            en welzijnssystemen innoveren en verduurzamen? Dit thema heeft betrekking op het leven en verblijven in de
-            gezonde regio. En dan gaat het vooral over de gezonde regio Zeeland.</p>
-        <em>Healthy lifestyle</em><br>
-        <p>Beweging en sport kunnen gezien worden als een fundament voor een gezonde leefstijl. Hoe kan in de gezonde
-            regio een actieve leefstijl gestimuleerd worden? Voor zowel bewoners als voor toeristen? Welk effect heeft
-            de aanwezigheid van sport- en beweegaanbod in de gezonde regio? En welke technologieën kunnen daarvoor
-            ingezet worden? Binnen dit thema wordt bewegen en sport gerelateerd aan vitaliteit, welzijn en gezond leven
-            en ouder worden.</p>
-        <em>Healthy hospitality<br></em>
-        <p>Hoe kunnen toerisme en gezondheidszorg aan elkaar gekoppeld worden? Zodat er nieuwe mogelijkheden ontstaan om
-            Zeeland als gezonde regio op de kaart te zetten? Slimme combinaties tussen zorg en toerisme, zodat zowel
-            burgers als toeristen gebruik kunnen maken van betere dienstverlening en producten? In dit thema worden twee
-            zeer diverse sectoren met elkaar verbonden. </p>
-        <p><img src="{{asset('healthyregion.jpg')}}"> </p>
-        </div>
-    </div>
 @endsection
