a775b18355a179586e698af6e186b504bfe931a3
bart0040 <bart0040@hz.nl>
2021-06-11 10:35:58 +0200

    comments en netheid

--- resources/views/documents/create.blade.php
+++ resources/views/documents/create.blade.php
@@ -182,30 +182,5 @@
             value="Cancel">
     </form>
 
-    <script>
-        /**
-         * this function adds a keyword input row
-         *
-         */
-        function addKeyword() {
-            document.getElementById("keywordsTable").insertRow().innerHTML =
-                "<tr><td>" +
-                "<input class='input' name='keywords_name[]' id='keywords' type='text' placeholder='Keyword here please...' value=''>" +
-                "</td><td>" +
-                "</td><td><button type='button' class='button' id='button2' onclick='deleteRow(this)'>-</button>" +
-                "</td></tr>";
-        }
-
-        /**
-         * this function deletes an added keyword input row
-         *
-         */
-        function deleteRow(r) {
-            let i = r.parentNode.parentNode.rowIndex;
-            document.getElementById("keywordsTable").deleteRow(i);
-
-        }
-    </script>
-
 
 @endsection
